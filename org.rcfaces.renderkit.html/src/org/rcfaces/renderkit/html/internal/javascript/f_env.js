7f766fd48233f7ae52a93a357f7108e2b22a0e08
flefevere <>
2007-05-04 13:28:55 +0000

    Ajout de la méthode public static ResolveContentUrl pour obtenir un url complet à partir d'une Window et d'un url de contenu supprimé les acarctéristiques final sur les méthodes static

--- org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/javascript/f_env.js
+++ org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/javascript/f_env.js
@@ -85,7 +85,7 @@ var __static = {
 	*/
 	
 	/**
-	 * @method public static final 
+	 * @method public static  
 	 * @param String name Name of the key.
 	 * @param any val Value to associate to the specified key
 	 * @return any Previous value.
@@ -99,7 +99,7 @@ var __static = {
 	},
 	
 	/**
-	 * @method public static final 
+	 * @method public static  
 	 * @param String name
 	 * @param any defaultValue
 	 * @return any Value associated to name, or specified default value.
@@ -116,7 +116,7 @@ var __static = {
 	},
 	
 	/**
-	 * @method public static final 
+	 * @method public static  
 	 * @param String name
 	 * @return any Previous value associated to specified name.
 	 */
@@ -132,14 +132,14 @@ var __static = {
 	},
 
 	/**
-	 * @method hidden static final 
+	 * @method hidden static  
 	 */
 	GetBaseURI: function() {
 		return f_env._BaseURI;
 	},
 	
 	/**
-	 * @method hidden static final 
+	 * @method hidden static  
 	 */
 	ComputeJavaScriptURI: function(uri) {
 		f_core.Assert(typeof(uri)=="string", "f_env.ComputeJavaScriptURI: invalid uri parameter '"+uri+"'.");
@@ -151,7 +151,7 @@ var __static = {
 		return baseURI+"/"+uri;
 	},
 	/**
-	 * @method hidden static final 
+	 * @method hidden static  
 	 * @return String The url of the rcfaces resources, or null if it is not defined !
 	 */
 	GetStyleSheetBase: function() {
@@ -169,14 +169,14 @@ var __static = {
 		return null;
 	},
 	/**
-	 * @method hidden static final 
+	 * @method hidden static  
 	 */
 	GetLocaleName: function() {
 		return f_env._LocaleName;
 	},
 	
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 */
 	Initialize: function(baseURI, viewURI, localeName, jsBaseURI, styleSheetBaseURI) {
 		f_env._BaseURI=baseURI;		
@@ -209,28 +209,28 @@ var __static = {
 	},
 	
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 */
 	GetCancelExternalSubmit: function() {
 		return f_env.Get(f_env._CANCEL_EXTERNAL_SUBMIT, false);
 	},
 	
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 */
 	EnableSubmitLockUntilPageComplete: function() {
 		f_env._LockSubmitUntilPageComplete=true;
 	},
 	
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 */
 	IsSubmitUntilPageCompleteLocked: function() {
 		return f_env._LockSubmitUntilPageComplete;
 	},
 		
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 */
 	GetViewURI: function() {
 		var uri=f_env._ViewURI;
@@ -242,14 +242,14 @@ var __static = {
 	},
 	
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 */
 	GetCheckValidation: function() {
 		return (f_env._ClientValidationDisabled!==true);
 	},
 	
 	/**
-	 * @method public static final
+	 * @method public static 
 	 */
 	EnableSensitiveCaseTagName: function() {
 		if (f_core.IsGecko()) { // Gecko n'est pas sensitive case !
@@ -261,7 +261,7 @@ var __static = {
 	},
 	
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 */
 	GetOpenWindowErrorMessage: function() {
 		var bundle=f_resourceBundle.Get(f_env);
@@ -272,7 +272,7 @@ var __static = {
 		return bundle.f_get("OPEN_WINDOW_ERROR_MESSAGE");
 	},
 	/**
-	 * @method hidden static final
+	 * @method hidden static 
 	 * @return String the domain
 	 */
 	GetDomain: function() {
@@ -314,6 +314,61 @@ var __static = {
 		
 		f_core.Debug(f_env, "Set domain to '"+domain+"' (url='"+url+"')");
 		return domain;
+	},
+     /**
+     * @method public static
+     * @param HTMLElement myWindow a window with the right location
+     * @param string url for the content
+     * @return string fully formed url
+     */
+    ResolveContentUrl: function(myWindow, url) {
+    	f_core.Assert(typeof(myWindow)=="object", "f_modalShell.ResolveContentUrl: Invalid parameter window '"+myWindow+"'."+typeof(myWindow));
+    	f_core.Assert(myWindow.location, "f_modalShell.ResolveContentUrl: Invalid parameter window has no location '"+myWindow+"'.");
+    	f_core.Assert((typeof(url)=="string"), "f_modalShell.ResolveContentUrl: Invalid parameter url '"+url+"'.");
+	    f_core.Debug(f_modalShell, "ResolveContentUrl : entering with ("+myWindow+", "+url+")");
+	    
+    	// Check for protocol in url
+    	var pos=url.indexOf(":");
+    	if (pos>0 && pos<url.indexOf("/")) {
+		    f_core.Debug(f_modalShell, "ResolveContentUrl : url already formed");
+    		return url;
+    	}
+    	// check for window protocol
+    	var windowUrl=myWindow.location.toString();
+    	pos=windowUrl.indexOf("//");
+    	if (pos<0) {
+		    f_core.Debug(f_modalShell, "ResolveContentUrl : window.location does not have a protocol ...");
+    		return url;
+    	}
+    	// extract protocol://domain:port from windowUrl into base and rest into remain
+    	pos=windowUrl.indexOf("/",pos+2);
+    	var base=windowUrl;
+    	var remain="";
+    	if (pos>=0) {
+	    	base=windowUrl.substring(0,pos);
+	    	remain=windowUrl.substring(pos+1);
+    	}
+    	// If url absolute
+    	if (url.indexOf("/")==0) {
+    		return base+url;
+    	}
+    	//if no remain
+    	if (remain.length==0) {
+    		return base+"/"+url;
+    	}
+    	// extract the last par of remain (page)
+    	var remains=remain.split("/");
+    	if (remain.charAt(remain.length-1) != "/") {
+    		remains.pop();
+    	}
+    	// del .. from url
+    	var adds=url.split("/");
+    	while (adds.length>0 && remains.length>0 && adds[0]=="..") {
+    		adds.shift();
+    		remains.pop();
+    	}
+    	//reconstruct complete url
+    	return base+"/"+remains.concat(adds).join("/");
 	},
 	/** 
 	 * @method hidden static
