aead951e243613f1e025961dc9c6b22465ed9ebb
oeuillot <>
2008-05-07 15:21:08 +0000

    Nouvelle interface pour généraliser la possibilité de formatter une date

---
+++ org.rcfaces.core/src/org/rcfaces/core/component/capability/IDateFormatCapability.java
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.core.component.capability;
+
+/**
+ * 
+ * @author Olivier Oeuillot (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public interface IDateFormatCapability {
+    String getDateFormat();
+
+    void setDateFormat(String dateFormat);
+}
