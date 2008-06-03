e70688421f2c67358389dbf347ad7163443499f3
oeuillot <>
2008-06-03 14:50:15 +0000

    L'interface n'hérite plus de IClientDataCapability

--- org.rcfaces.core/src-jsf1_2/org/rcfaces/core/internal/manager/IClientDataManager.java
+++ org.rcfaces.core/src-jsf1_2/org/rcfaces/core/internal/manager/IClientDataManager.java
@@ -6,13 +6,13 @@ package org.rcfaces.core.internal.manager;
 
 import javax.el.ValueExpression;
 
-import org.rcfaces.core.component.capability.IClientDataCapability;
-
 /**
  * 
  * @author Olivier Oeuillot (latest modification by $Author$)
  * @version $Revision$ $Date$
  */
-public interface IClientDataManager extends IClientDataCapability {
+public interface IClientDataManager {
+    String setClientData(String name, String value);
+
     void setClientData(String name, ValueExpression binding);
 }
