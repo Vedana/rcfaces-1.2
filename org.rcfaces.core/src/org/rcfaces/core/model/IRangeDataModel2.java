4ae1babfb3c405b5df1abd3da674fc66963ef743
oeuillot <>
2008-06-03 14:52:28 +0000

    Extension de l'interface IRangeDataModel, avec un parametre en plus qui permet de savoir si RCFaces appelera une ligne supplémentaire pour tester la fin de la liste

---
+++ org.rcfaces.core/src/org/rcfaces/core/model/IRangeDataModel2.java
@@ -0,0 +1,12 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.core.model;
+
+/**
+ * @author Olivier Oeuillot (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public interface IRangeDataModel2 {
+    void setRowRange(int index, int length, boolean testLastRow);
+}
