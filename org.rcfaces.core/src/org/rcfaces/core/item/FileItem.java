df5ef4ceaa882200b132654de9df488475cb8882
oeuillot <>
2009-03-04 10:10:37 +0000

    Ajout d'un parametre: charSet

--- org.rcfaces.core/src/org/rcfaces/core/item/FileItem.java
+++ org.rcfaces.core/src/org/rcfaces/core/item/FileItem.java
@@ -6,6 +6,8 @@ package org.rcfaces.core.item;
 import javax.faces.component.UISelectItem;
 import javax.faces.model.SelectItem;
 
+import org.rcfaces.core.component.FileItemComponent;
+
 /**
  * 
  * @author Olivier Oeuillot (latest modification by $Author$)
@@ -16,16 +18,25 @@ public class FileItem extends SelectItem implements IFileItem {
 
     private static final long serialVersionUID = -691526095374431782L;
 
+    private String charSet;
+
     public FileItem() {
     }
 
     public FileItem(IFileItem fileItem) {
         super(fileItem.getSrc(), null, null, false);
+
+        this.charSet = fileItem.getCharSet();
     }
 
     public FileItem(UISelectItem component) {
         super(component.getItemValue(), component.getItemLabel(), component
                 .getItemDescription(), component.isItemDisabled());
+
+        if (component instanceof FileItemComponent) {
+            charSet = ((FileItemComponent) component).getCharSet();
+        }
+
     }
 
     public void setSrc(String src) {
@@ -35,4 +46,13 @@ public class FileItem extends SelectItem implements IFileItem {
     public String getSrc() {
         return (String) getValue();
     }
+
+    public String getCharSet() {
+        return charSet;
+    }
+
+    public void setCharSet(String charSet) {
+        this.charSet = charSet;
+    }
+
 }
--- org.rcfaces.core/src/org/rcfaces/core/item/ICharSetItem.java
+++ org.rcfaces.core/src/org/rcfaces/core/item/ICharSetItem.java
@@ -8,6 +8,6 @@ package org.rcfaces.core.item;
  * @author Olivier Oeuillot (latest modification by $Author$)
  * @version $Revision$ $Date$
  */
-public interface IFileItem extends ICharSetItem {
-    String getSrc();
+public interface ICharSetItem {
+    String getCharSet();
 }
