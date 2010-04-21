06d35eac53f60a693a3364c472c82e8b284fd5cf
oeuillot <>
2010-04-21 13:57:47 +0000

    Ajout du renderer du tabNamingContainer

---
+++ org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/renderer/TabNamingContainerRenderer.java
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.renderkit.html.internal.renderer;
+
+/**
+ * 
+ * @author Olivier Oeuillot (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public class TabNamingContainerRenderer extends TabRenderer {
+    private static final String REVISION = "$Revision$";
+
+}
