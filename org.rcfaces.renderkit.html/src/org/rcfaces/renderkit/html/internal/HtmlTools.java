8a59f6d6a4a704fcb57d045bdb31729ca2708bcc
oeuillot <>
2010-06-22 10:06:03 +0000

    Retire le ms ... on ne peut pas encoder des LONG contenant des ms !

--- org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/HtmlTools.java
+++ org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/HtmlTools.java
@@ -771,7 +771,7 @@ public class HtmlTools {
 				hours = calendar.get(Calendar.HOUR_OF_DAY);
 				minutes = calendar.get(Calendar.MINUTE);
 				seconds = calendar.get(Calendar.SECOND);
-				millis = calendar.get(Calendar.MILLISECOND);
+//				millis = calendar.get(Calendar.MILLISECOND);
 			}
 		}
 
