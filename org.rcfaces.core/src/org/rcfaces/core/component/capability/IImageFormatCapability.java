c815becf1b8629dd9b090db9232a86e8477b2e82
oeuillot <>
2010-08-10 08:31:56 +0000

    Nouvelle capability

---
+++ org.rcfaces.core/src/org/rcfaces/core/component/capability/IImageFormatCapability.java
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.core.component.capability;
+
+/**
+ * 
+ * @author Olivier Oeuillot (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public interface IImageFormatCapability {
+    String getImageFormat();
+
+    void setImageFormat(String imageFormat);
+}
