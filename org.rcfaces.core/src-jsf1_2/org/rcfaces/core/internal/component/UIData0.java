418e8673c9aea333c65c25bef64f57908361eab5
jbmeslin <>
2010-09-28 08:33:57 +0000

    Correction null pointer exception de la fonction getFirst erreur si rows est null

--- org.rcfaces.core/src-jsf1_2/org/rcfaces/core/internal/component/UIData0.java
+++ org.rcfaces.core/src-jsf1_2/org/rcfaces/core/internal/component/UIData0.java
@@ -268,7 +268,12 @@ public class UIData0 extends UIComponentBase implements NamingContainer {
 				throw new FacesException(e);
 			}
 			if (null == value) {
-				return first;
+				if(first == null){
+					return 0;
+				}else {
+					return first.intValue();
+				}
+				
 			}
 			return (value.intValue());
 		} else {
