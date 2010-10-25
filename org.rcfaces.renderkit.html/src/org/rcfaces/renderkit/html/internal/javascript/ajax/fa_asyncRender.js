9ee7c9fa5117d44195a23a38df31481d15f95d19
jbmeslin <>
2010-10-25 11:25:02 +0000

    Correction preSelection dans un onglet en mode asynchrone

--- org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/javascript/ajax/fa_asyncRender.js
+++ org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/javascript/ajax/fa_asyncRender.js
@@ -36,145 +36,168 @@ var __members = {
 		if (!this._interactive) {
 			return true;
 		}
-		this._interactive=undefined;
+		var component= this;
+		var waitSubmit = function () {
+ 			if (window._rcfacesExiting) {
+ 				return false;
+ 			}
+ 			var lock = f_event.GetEventLocked(null, false, f_event.SUBMIT_LOCK);
+ 			
+ 			if(lock){
+ 				callAsync();
+ 			}else {
+ 				return;
+ 			}
+ 			
+		};
 		
-		var url=f_env.GetViewURI();
-		var request=new f_httpRequest(this, url, f_httpRequest.TEXT_HTML_MIME_TYPE);
-		var component=this;
-		if (!parent) {
-			if (typeof(this.fa_getInteractiveParent)=="function") {
-				parent=this.fa_getInteractiveParent();
-			}
+		this._interactive=undefined;
+		window.setTimeout(waitSubmit, 12);
+		
+		
+		
+		
+		var callAsync = function () {
+ 			if (window._rcfacesExiting) {
+ 				return false;
+ 			}
+			var url=f_env.GetViewURI();
+			var request=new f_httpRequest(component, url, f_httpRequest.TEXT_HTML_MIME_TYPE);
 			
 			if (!parent) {
-				parent=component;
+				if (typeof(component.fa_getInteractiveParent)=="function") {
+					parent=component.fa_getInteractiveParent();
+				}
+				
+				if (!parent) {
+					parent=component;
+				}
 			}
-		}
-		
-		if (!component.style.height || component.offsetHeight<f_waiting.HEIGHT) {
-			component._removeStyleHeight=true;
-			component.style.height=f_waiting.HEIGHT+"px";
-		}
-		
-		request.f_setListener({
-	 		onInit: function(request) {
-	 			var waiting=component._intWaiting;
-	 			if (!waiting) {	
-	 				waiting=f_waiting.Create(parent);
-	 				component._intWaiting=waiting;
-	 			}
-	 			
-	 			waiting.f_setText(f_waiting.GetLoadingMessage());
-	 			waiting.f_show();
-	 		},
-			/* *
-			 * @method public
-			 */
-	 		onError: function(request, status, text) {
-				component._intLoading=false;		
-				
-	 			var waiting=component._intWaiting;
-				if (waiting) {
-					waiting.f_hide();
-				}
-				f_core.Info(fa_asyncRender, "f_prepare.onError: Bad status: "+status);
-				
-				component.f_performAsyncErrorEvent(request, f_error.HTTP_ERROR, text);
-	 		},
-			/* *
-			 * @method public
-			 */
-	 		onProgress: function(request, content, length, contentType) {
-	 			var waiting=component._intWaiting;
-				if (waiting) {
-					waiting.f_setText(f_waiting.GetReceivingMessage());
-				}	 			
-	 		},
-			/* *
-			 * @method public
-			 */
-	 		onLoad: function(request, content, contentType) {			
-				if (component._removeStyleHeight) {
-					component._removeStyleHeight=null;
-					if (component.style.height==f_waiting.HEIGHT+"px") {
-						component.style.height="auto";
-					}
-				}
-	
-
-	 			var waiting=component._intWaiting;
-				try {
-					if (request.f_getStatus()!=f_httpRequest.OK_STATUS) {
-						component.f_performAsyncErrorEvent(request, f_error.INVALID_RESPONSE_ASYNC_RENDER_ERROR, "Bad http response status ! ("+request.f_getStatusText()+")");
-						return;
-					}
-
-					var cameliaServiceVersion=request.f_getResponseHeader(f_httpRequest.CAMELIA_RESPONSE_HEADER);
-					if (!cameliaServiceVersion) {
-						component.f_performAsyncErrorEvent(request, f_error.INVALID_SERVICE_RESPONSE_ERROR, "Not a service response !");
-						return;					
-					}
+			
+			if (!component.style.height || component.offsetHeight<f_waiting.HEIGHT) {
+				component._removeStyleHeight=true;
+				component.style.height=f_waiting.HEIGHT+"px";
+			}
+			
+			request.f_setListener({
+		 		onInit: function(request) {
+		 			var waiting=component._intWaiting;
+		 			if (!waiting) {	
+		 				waiting=f_waiting.Create(parent);
+		 				component._intWaiting=waiting;
+		 			}
+		 			
+		 			waiting.f_setText(f_waiting.GetLoadingMessage());
+		 			waiting.f_show();
+		 		},
+				/* *
+				 * @method public
+				 */
+		 		onError: function(request, status, text) {
+					component._intLoading=false;		
 					
-					var ret=request.f_getResponse();
-					//	alert("Ret="+ret);
-
-					var responseContentType=request.f_getResponseContentType().toLowerCase();
-					if (responseContentType.indexOf(f_error.APPLICATION_ERROR_MIME_TYPE)>=0) {
-						var code=f_error.ComputeApplicationErrorCode(request);
-				
-				 		component.f_performErrorEvent(request, code, content);
-						return;
-					}
-				
-					if (responseContentType.indexOf(f_httpRequest.JAVASCRIPT_MIME_TYPE)>=0) {
-						try {
-							f_core.WindowScopeEval(ret);
-							
-						} catch (x) {
-				 			component.f_performAsyncErrorEvent(x, f_error.RESPONSE_EVALUATION_ASYNC_RENDER_ERROR, "Evaluation exception");
-						}
-						
-						component.fa_contentLoaded(ret, responseContentType, parent);
-						return;
-					}
-					
-					if (responseContentType.indexOf(f_httpRequest.TEXT_HTML_MIME_TYPE)>=0) {
-						if (waiting) {
-							waiting.f_hide();
-							waiting.f_close();
-							waiting=null;
-						}
-						
-						try {
-							component.f_getClass().f_getClassLoader().f_loadContent(component, component, component.innerHTML+ret);
-							
-							component.fa_contentLoaded(ret, responseContentType, parent);
-						} catch (x) {
-				 			component.f_performAsyncErrorEvent(x, f_error.RESPONSE_EVALUATION_ASYNC_RENDER_ERROR, "Evaluation exception");
-						}
-						return;
-					}
-					
-				 	component.f_performAsyncErrorEvent(request, f_error.RESPONSE_TYPE_ASYNC_RENDER_ERROR, "Unsupported content type: "+responseContentType);
-					
-				} finally {				
-					component._intLoading=false;
-
+		 			var waiting=component._intWaiting;
 					if (waiting) {
 						waiting.f_hide();
 					}
-				}
-	 		}			
-		});
-
-		this._intLoading=true;
-		request.f_setRequestHeader("X-Camelia", "asyncRender.request");
-
-		var	param={
-			id: this.id
-		};
-		request.f_doFormRequest(param);
+					f_core.Info(fa_asyncRender, "f_prepare.onError: Bad status: "+status);
+					
+					component.f_performAsyncErrorEvent(request, f_error.HTTP_ERROR, text);
+		 		},
+				/* *
+				 * @method public
+				 */
+		 		onProgress: function(request, content, length, contentType) {
+		 			var waiting=component._intWaiting;
+					if (waiting) {
+						waiting.f_setText(f_waiting.GetReceivingMessage());
+					}	 			
+		 		},
+				/* *
+				 * @method public
+				 */
+		 		onLoad: function(request, content, contentType) {			
+					if (component._removeStyleHeight) {
+						component._removeStyleHeight=null;
+						if (component.style.height==f_waiting.HEIGHT+"px") {
+							component.style.height="auto";
+						}
+					}
 		
+	
+		 			var waiting=component._intWaiting;
+					try {
+						if (request.f_getStatus()!=f_httpRequest.OK_STATUS) {
+							component.f_performAsyncErrorEvent(request, f_error.INVALID_RESPONSE_ASYNC_RENDER_ERROR, "Bad http response status ! ("+request.f_getStatusText()+")");
+							return;
+						}
+	
+						var cameliaServiceVersion=request.f_getResponseHeader(f_httpRequest.CAMELIA_RESPONSE_HEADER);
+						if (!cameliaServiceVersion) {
+							component.f_performAsyncErrorEvent(request, f_error.INVALID_SERVICE_RESPONSE_ERROR, "Not a service response !");
+							return;					
+						}
+						
+						var ret=request.f_getResponse();
+						//	alert("Ret="+ret);
+	
+						var responseContentType=request.f_getResponseContentType().toLowerCase();
+						if (responseContentType.indexOf(f_error.APPLICATION_ERROR_MIME_TYPE)>=0) {
+							var code=f_error.ComputeApplicationErrorCode(request);
+					
+					 		component.f_performErrorEvent(request, code, content);
+							return;
+						}
+					
+						if (responseContentType.indexOf(f_httpRequest.JAVASCRIPT_MIME_TYPE)>=0) {
+							try {
+								f_core.WindowScopeEval(ret);
+								
+							} catch (x) {
+					 			component.f_performAsyncErrorEvent(x, f_error.RESPONSE_EVALUATION_ASYNC_RENDER_ERROR, "Evaluation exception");
+							}
+							
+							component.fa_contentLoaded(ret, responseContentType, parent);
+							return;
+						}
+						
+						if (responseContentType.indexOf(f_httpRequest.TEXT_HTML_MIME_TYPE)>=0) {
+							if (waiting) {
+								waiting.f_hide();
+								waiting.f_close();
+								waiting=null;
+							}
+							
+							try {
+								component.f_getClass().f_getClassLoader().f_loadContent(component, component, component.innerHTML+ret);
+								
+								component.fa_contentLoaded(ret, responseContentType, parent);
+							} catch (x) {
+					 			component.f_performAsyncErrorEvent(x, f_error.RESPONSE_EVALUATION_ASYNC_RENDER_ERROR, "Evaluation exception");
+							}
+							return;
+						}
+						
+					 	component.f_performAsyncErrorEvent(request, f_error.RESPONSE_TYPE_ASYNC_RENDER_ERROR, "Unsupported content type: "+responseContentType);
+						
+					} finally {				
+						component._intLoading=false;
+	
+						if (waiting) {
+							waiting.f_hide();
+						}
+					}
+		 		}			
+			});
+	
+			this._intLoading=true;
+			request.f_setRequestHeader("X-Camelia", "asyncRender.request");
+	
+			var	param={
+				id: this.id
+			};
+			request.f_doFormRequest(param);
+		}
 		return false;
 	},
 	/**
