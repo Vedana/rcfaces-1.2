9d2054b96d54bfe889ed8766b7406f9e143e7904
oeuillot <>
2011-11-10 10:31:35 +0000

    Mise en page

--- org.rcfaces.core/src/org/rcfaces/core/lang/AdaptedDataModel.java
+++ org.rcfaces.core/src/org/rcfaces/core/lang/AdaptedDataModel.java
@@ -5,6 +5,10 @@ import javax.faces.model.DataModel;
 import org.rcfaces.core.internal.RcfacesContext;
 import org.rcfaces.core.internal.adapter.IAdapterManager;
 
+/**
+ * 
+ * @author Olivier Oeuillot
+ */
 public abstract class AdaptedDataModel extends DataModel implements IAdaptable {
 
 	public Object getAdapter(Class adapter, Object parameter) {
