23acfbfbed8dc6db76f1e296f0ff899d567d644d
oeuillot <>
2012-06-11 09:03:01 +0000

    Mise en page

--- org.rcfaces.core/src/org/rcfaces/core/internal/component/AbstractPropertiesManager.java
+++ org.rcfaces.core/src/org/rcfaces/core/internal/component/AbstractPropertiesManager.java
@@ -9,6 +9,5 @@ package org.rcfaces.core.internal.component;
  * @version $Revision$ $Date$
  */
 public abstract class AbstractPropertiesManager implements IPropertiesManager {
-    private static final String REVISION = "$Revision$";
 
 }
