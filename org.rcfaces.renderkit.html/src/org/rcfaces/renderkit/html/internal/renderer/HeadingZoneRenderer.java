f094e48f01b3cbcb10dd79dd389fd65f5eeeda24
oeuillot <>
2012-07-04 12:56:55 +0000

    Caption d'un HeadingZone

--- org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/renderer/HeadingZoneRenderer.java
+++ org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/renderer/HeadingZoneRenderer.java
@@ -3,6 +3,10 @@
  */
 package org.rcfaces.renderkit.html.internal.renderer;
 
+import java.util.List;
+
+import javax.faces.component.UIComponent;
+
 import org.rcfaces.core.component.HeadingZoneComponent;
 import org.rcfaces.core.internal.renderkit.IComponentWriter;
 import org.rcfaces.core.internal.renderkit.WriterException;
@@ -21,8 +25,6 @@ public class HeadingZoneRenderer extends AbstractCssRenderer {
     public final void encodeBegin(IComponentWriter writer)
             throws WriterException {
 
-        super.encodeBegin(writer);
-
         IHtmlWriter htmlWriter = (IHtmlWriter) writer;
 
         HeadingZoneComponent headingZoneComponent = (HeadingZoneComponent) writer
@@ -30,7 +32,31 @@ public class HeadingZoneRenderer extends AbstractCssRenderer {
 
         String text = headingZoneComponent.getCaption(writer
                 .getComponentRenderContext().getFacesContext());
+
+        boolean hasCaption = false;
         if (text != null && text.length() > 0) {
+            hasCaption = true;
+
+            boolean visible = true;
+            List<UIComponent> children = headingZoneComponent.getChildren();
+            for (UIComponent child : children) {
+
+                if (child.isRendered() == false) {
+                    continue;
+                }
+
+                visible = true;
+                break;
+            }
+
+            if (visible == false) {
+                headingZoneComponent.setRendered(visible);
+            }
+        }
+
+        super.encodeBegin(writer);
+
+        if (hasCaption) {
             int ariaLevel = HeadingTools
                     .computeHeadingLevel(headingZoneComponent);
 
