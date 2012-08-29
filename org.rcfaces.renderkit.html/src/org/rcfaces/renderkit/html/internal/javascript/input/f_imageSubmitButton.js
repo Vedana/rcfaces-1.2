c0e17caa93cad9aac4a21599d913297b1af389b7
oeuillot <>
2012-08-29 15:02:30 +0000

    Correction du double submit

--- org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/javascript/input/f_imageSubmitButton.js
+++ org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/javascript/input/f_imageSubmitButton.js
@@ -10,6 +10,14 @@
  * @version $Revision$ $Date$
  */
 var __members = {
+
+	f_imageSubmitButton: function() {
+		this.f_super(arguments);
+		
+		//this.onmousedown=f_core.CancelJsEventHandler;
+		// this.onclick=f_core.CancelJsEventHandler;
+	},
+	
 	/**
 	 * @method protected
 	 * @param f_event event
@@ -25,6 +33,26 @@ var __members = {
 			f_core.Error(f_imageSubmitButton, "f_performImageSelection: FORM component was not found !");
 		}
 	*/
+		
+		if (window._rcfacesExiting) {
+			return false;
+		}
+		
+		if (window.f_shellManager) {
+			var shell=f_shellManager.GetShell(this);
+			if (shell) {
+				var status=shell.f_getStatus();
+				
+				switch(status) {
+				case f_shell.CLOSING_STATUS:
+				case f_shell.ABOUT_TO_CLOSE_STATUS:
+				case f_shell.CLOSED_STATUS:
+				case f_shell.DESTROYING_STATUS:
+					return false;
+				}				
+			}
+		}
+		
 		f_core.SubmitEvent(event);
 				
 		return false;
