c1f8b33acf8a14b720a21c7302f05f19da928f66
oeuillot <>
2013-01-08 16:54:20 +0000

    Nouveau LookId pour les ComponentsListRenderer

---
+++ org.rcfaces.renderkit.html/src/org/rcfaces/renderkit/html/internal/renderer/ComponentsListULRenderer.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.renderkit.html.internal.renderer;
+
+import javax.faces.context.FacesContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.rcfaces.core.component.ComponentsListComponent;
+import org.rcfaces.core.internal.renderkit.WriterException;
+import org.rcfaces.renderkit.html.internal.IHtmlWriter;
+
+/**
+ * 
+ * @author Olivier Oeuillot (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public class ComponentsListULRenderer extends AbstractComponentsListRenderer {
+
+    private static final Log LOG = LogFactory
+            .getLog(ComponentsListULRenderer.class);
+
+    protected void encodeListBegin(IHtmlWriter htmlWriter,
+            ComponentsListComponent componentsListComponent,
+            ListContext listContext) throws WriterException {
+
+        FacesContext facesContext = htmlWriter.getComponentRenderContext()
+                .getFacesContext();
+
+        htmlWriter.startElement(IHtmlWriter.UL);
+
+        htmlWriter.writeId(htmlWriter.getComponentRenderContext()
+                .getComponentClientId() + "::table");
+
+        String w = componentsListComponent.getWidth(facesContext);
+        if (w != null) {
+            htmlWriter.writeStyle().writeWidth("100%");
+        }
+
+        htmlWriter.writeClass(getTBodyClassName(htmlWriter));
+    }
+
+    protected void encodeComponentBegin(IHtmlWriter htmlWriter,
+            ComponentsListComponent componentsListComponent, int processed,
+            int columnNumber, String[] rowClasses, String tdClass)
+            throws WriterException {
+
+        htmlWriter.startElement(IHtmlWriter.LI);
+        htmlWriter.writeAttributeNS("nc", true);
+        htmlWriter.writeClass(tdClass);
+
+        htmlWriter.writeln();
+
+    }
+
+    protected void encodeComponentEnd(IHtmlWriter htmlWriter,
+            ComponentsListComponent componentsListComponent, int processed,
+            int columnNumber, String[] rowClasses, String tdClass)
+            throws WriterException {
+
+        htmlWriter.endElement(IHtmlWriter.LI);
+    }
+
+    protected void encodeChildrenComponentEnd(IHtmlWriter htmlWriter,
+            ComponentsListComponent componentsListComponent, int processed,
+            int columnNumber, String[] rowClasses, String tdClass)
+            throws WriterException {
+    }
+
+    protected void encodeListEnd(IHtmlWriter htmlWriter,
+            ComponentsListComponent componentsListComponent)
+            throws WriterException {
+        htmlWriter.endElement(IHtmlWriter.UL);
+    }
+
+}
