99710bb7a71488ada158846d4a4c2786a352ca80
oeuillot <>
2013-03-21 10:35:44 +0000

    OutlinedLael

---
+++ org.rcfaces.core/src/org/rcfaces/core/component/capability/IOutlinedLabelCapability.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ * 
+ */
+package org.rcfaces.core.component.capability;
+
+/**
+ * A label which will be outlined
+ * 
+ * @version $Revision$ $Date$
+ */
+public interface IOutlinedLabelCapability {
+
+    public enum Method {
+        IgnoreCase,
+        IgnoreAccents,
+        Multiple,
+        StartsWith,
+        WordOnly,
+        FullText
+    }
+    
+    /**
+     * Returns the label which is outlined
+     * 
+     * @return The label
+     */
+    String getOutlinedLabel();
+
+    /**
+     * Sets the label which will be outlined
+     * 
+     * @param label
+     *            Label which is outlined
+     */
+    void setOutlinedLabel(String label);
+
+    String getOutlinedLabelMethod();
+
+    void setOutlinedLabelMethod(String method);
+}
