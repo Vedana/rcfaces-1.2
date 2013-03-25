297d9a57e0aab64d4937f794f8c380c39b41c662
oeuillot <>
2013-03-25 14:56:46 +0000

    Renomme l'interface

--- org.rcfaces.core/src/org/rcfaces/core/component/iterator/ISelectItemFinder.java
+++ org.rcfaces.core/src/org/rcfaces/core/component/iterator/ISelectItemFinder.java
@@ -3,12 +3,10 @@
  */
 package org.rcfaces.core.component.iterator;
 
-import java.util.EnumSet;
 import java.util.Map;
 
 import javax.faces.component.UIComponent;
 
-import org.rcfaces.core.component.capability.IOutlinedLabelCapability;
 import org.rcfaces.core.item.ISelectItemPath;
 
 /**
@@ -16,8 +14,7 @@ import org.rcfaces.core.item.ISelectItemPath;
  * @author Olivier Oeuillot (latest modification by $Author$)
  * @version $Revision$ $Date$
  */
-public interface ISearchSelectItem {
+public interface ISelectItemFinder {
     ISelectItemPath[] search(UIComponent component,
-            Map<String, Object> criterias,
-            EnumSet<IOutlinedLabelCapability.Method> methods);
+            Map<String, Object> filterProperies);
 }
