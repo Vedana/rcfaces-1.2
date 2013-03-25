fe9f4a092b738e15880403e4218832d234d65b90
oeuillot <>
2013-03-25 14:57:08 +0000

    Split du TOOLS

--- org.rcfaces.core/src/org/rcfaces/core/util/SelectItemTools.java
+++ org.rcfaces.core/src/org/rcfaces/core/util/SelectItemTools.java
@@ -3,12 +3,8 @@
  */
 package org.rcfaces.core.util;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 import javax.faces.component.UIComponent;
 import javax.faces.component.UISelectItem;
@@ -24,10 +20,8 @@ import org.rcfaces.core.internal.Constants;
 import org.rcfaces.core.internal.RcfacesContext;
 import org.rcfaces.core.item.BasicImagesSelectItem;
 import org.rcfaces.core.item.BasicSelectItem;
-import org.rcfaces.core.item.BasicSelectItemPath;
 import org.rcfaces.core.item.ISelectItem;
 import org.rcfaces.core.item.ISelectItemGroup;
-import org.rcfaces.core.item.ISelectItemPath;
 import org.rcfaces.core.lang.IAdaptable;
 import org.rcfaces.core.model.IFilterProperties;
 import org.rcfaces.core.model.IFiltredCollection;
@@ -43,58 +37,40 @@ public class SelectItemTools {
 
     private static final Log LOG = LogFactory.getLog(SelectItemTools.class);
 
-    private static final ISelectItemNodeFactory DEFAULT_FACTORY = new ISelectItemNodeFactory() {
+    public static <T> T traverseSelectItemTree(FacesContext facesContext,
+            UIComponent component, IFilterProperties filterProperties,
+            ISelectItemNodeHandler<T> handler) {
 
-        public SelectItemNode newSelectItemNode(SelectItemNode parent,
-                String id, SelectItem selectItem, int depth) {
-            return new SelectItemNode(parent, id, selectItem, depth);
-        }
-    };
-
-    public static SelectItemNode listSelectItems(UIComponent component) {
-        return listSelectItems(null, component, null, null, null);
-    }
-
-    public static SelectItemNode listSelectItems(FacesContext facesContext,
-            UIComponent component) {
-        return listSelectItems(facesContext, component, null, null, null);
-    }
-
-    public static SelectItemNode listSelectItems(FacesContext facesContext,
-            UIComponent component, ISelectItemNodeFactory factory,
-            IFilterProperties filterProperties, ISelectItemNodeHandler handler) {
         if (facesContext == null) {
             facesContext = FacesContext.getCurrentInstance();
         }
-        if (factory == null) {
-            factory = DEFAULT_FACTORY;
-        }
 
-        SelectItemWalker walker = new SelectItemWalker(facesContext, component,
-                factory, filterProperties, handler);
-
-        SelectItemNode root = factory.newSelectItemNode(null, null, null, 0);
+        SelectItemWalker<T> walker = new SelectItemWalker<T>(facesContext,
+                component, filterProperties, handler);
 
         if (handler != null) {
-            handler.startTree(root);
+            T t = handler.beginTree(component);
+            if (t != null) {
+                return t;
+            }
         }
 
         List<UIComponent> children = component.getChildren();
         for (UIComponent child : children) {
-            walker.listSelectItems(child, root);
+            T t = walker.processComponent(child);
+            if (t != null) {
+                return t;
+            }
         }
 
         if (handler != null) {
-            handler.endTree(root);
+            T t = handler.endTree(component);
+            if (t != null) {
+                return t;
+            }
         }
 
-        return root;
-    }
-
-    public static void traverseSelectItems(FacesContext facesContext,
-            UIComponent component, ISelectItemNodeHandler handler,
-            IFilterProperties properties) {
-
+        return null;
     }
 
     public static SelectItem convertToSelectItem(FacesContext facesContext,
@@ -148,9 +124,19 @@ public class SelectItemTools {
         return null;
     }
 
-    public interface ISelectItemNodeFactory {
-        SelectItemNode newSelectItemNode(SelectItemNode parent, String id,
-                SelectItem selectItem, int depth);
+    /**
+     * 
+     * @author Olivier Oeuillot (latest modification by $Author$)
+     * @version $Revision$ $Date$
+     */
+    public interface ISelectItemNodeHandler<T> {
+        T beginTree(UIComponent component);
+
+        T beginNode(UIComponent component, SelectItem selectItem);
+
+        T endNode(UIComponent component, SelectItem selectItem);
+
+        T endTree(UIComponent component);
     }
 
     /**
@@ -158,235 +144,74 @@ public class SelectItemTools {
      * @author Olivier Oeuillot (latest modification by $Author$)
      * @version $Revision$ $Date$
      */
-    public interface ISelectItemNodeHandler {
-        void startTree(SelectItemNode root);
+    public static class DefaultSelectItemNodeHandler<T> implements
+            ISelectItemNodeHandler<T> {
 
-        void beginNode(SelectItemNode node);
-
-        void endNode(SelectItemNode node);
-
-        void endTree(SelectItemNode root);
-    }
-
-    /**
-     * 
-     * @author Olivier Oeuillot (latest modification by $Author$)
-     * @version $Revision$ $Date$
-     */
-    public static class SelectItemNode {
-        private static final SelectItemNode[] EMPTY_ARRAY = new SelectItemNode[0];
-
-        private final SelectItem selectItem;
-
-        private List<SelectItemNode> children = null;
-
-        private final String id;
-
-        private final int depth;
-
-        private final SelectItemNode parent;
-
-        private ISelectItemPath path;
-
-        protected SelectItemNode(SelectItemNode parent, String id,
-                SelectItem selectItem, int depth) {
-            this.parent = parent;
-            this.selectItem = selectItem;
-            this.id = id;
-            this.depth = depth;
-        }
-
-        public SelectItemNode addChild(ISelectItemNodeFactory factory,
-                String id, SelectItem selectItem) {
-            SelectItemNode child = factory.newSelectItemNode(this, id,
-                    selectItem, depth + 1);
-
-            if (children == null) {
-                children = new ArrayList<SelectItemNode>();
-            }
-            children.add(child);
-
-            return child;
-        }
-
-        public SelectItem getSelectItem() {
-            return selectItem;
-        }
-
-        public SelectItemNode[] getChildren() {
-            if (children == null) {
-                return EMPTY_ARRAY;
-            }
-            return children.toArray(new SelectItemNode[children.size()]);
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public SelectItemNode getParent() {
-            return parent;
-        }
-
-        public int getDepth() {
-            return depth;
-        }
-
-        public SelectItemNode searchById(String id) {
-            if (id.equals(getId())) {
-                return this;
-            }
-
-            SelectItemNode chilren[] = getChildren();
-            for (int i = 0; i < chilren.length; i++) {
-                SelectItemNode si = chilren[i].searchById(id);
-                if (si != null) {
-                    return si;
-                }
-            }
-
+        public T beginTree(UIComponent component) {
             return null;
         }
 
-        public SelectItemNode searchByValue(Object value) {
-            if (selectItem != null && value.equals(selectItem.getValue())) {
-                return this;
-            }
-
-            SelectItemNode chilren[] = getChildren();
-            for (int i = 0; i < chilren.length; i++) {
-                SelectItemNode si = chilren[i].searchByValue(value);
-                if (si != null) {
-                    return si;
-                }
-            }
-
+        public T beginNode(UIComponent component, SelectItem selectItem) {
             return null;
         }
 
-        public SelectItemNode getRoot() {
-            SelectItemNode node = this;
-            for (; node.getParent() != null; node = node.getParent()) {
-            }
-
-            return node;
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + depth;
-            result = prime * result + ((id == null) ? 0 : id.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (!(obj instanceof SelectItemNode)) {
-                return false;
-            }
-            SelectItemNode other = (SelectItemNode) obj;
-            if (depth != other.depth) {
-                return false;
-            }
-            if (id == null) {
-                if (other.id != null) {
-                    return false;
-                }
-            } else if (!id.equals(other.id)) {
-                return false;
-            }
-            return true;
-        }
-
-        public ISelectItemPath getPath() {
-            if (path != null) {
-                return path;
-            }
-
-            List<SelectItem> segments = new LinkedList<SelectItem>();
-            for (SelectItemNode node = this; node != null; node = node
-                    .getParent()) {
-
-                segments.add(0, node.getSelectItem());
-            }
-
-            path = new BasicSelectItemPath(segments);
-
-            return path;
-        }
-    }
-
-    public static SelectItem searchId(SelectItemNode node, String id) {
-        SelectItemNode sn = node.searchById(id);
-        if (sn == null) {
+        public T endNode(UIComponent component, SelectItem selectItem) {
             return null;
         }
 
-        return sn.getSelectItem();
+        public T endTree(UIComponent component) {
+            return null;
+        }
     }
 
-    private static class SelectItemWalker {
+    private static class SelectItemWalker<T> {
 
         private final FacesContext facesContext;
 
         private final UIComponent mainComponent;
 
-        private final ISelectItemNodeFactory factory;
-
         private final IFilterProperties filterProperties;
 
-        private final ISelectItemNodeHandler handler;
-
-        private final Set<String> selectItemIds = new HashSet<String>();
+        private final ISelectItemNodeHandler<T> handler;
 
         public SelectItemWalker(FacesContext facesContext,
-                UIComponent mainComponent, ISelectItemNodeFactory factory,
-                IFilterProperties filterProperties,
-                ISelectItemNodeHandler handler) {
+                UIComponent mainComponent, IFilterProperties filterProperties,
+                ISelectItemNodeHandler<T> handler) {
 
             this.facesContext = facesContext;
             this.mainComponent = mainComponent;
-            this.factory = factory;
             this.filterProperties = filterProperties;
             this.handler = handler;
         }
 
-        protected void listSelectItems(UIComponent currentComponent,
-                SelectItemNode parentNode) {
-            String componentClientId = currentComponent
-                    .getClientId(facesContext);
+        protected T processComponent(UIComponent component) {
 
-            if (currentComponent instanceof UISelectItem) {
-                UISelectItem uiSelectItem = (UISelectItem) currentComponent;
-
-                SelectItemNode singleItemNode = null;
+            if (component instanceof UISelectItem) {
+                UISelectItem uiSelectItem = (UISelectItem) component;
 
                 Object value = uiSelectItem.getValue();
                 if (value instanceof SelectItem) {
-                    singleItemNode = addSelectItem(parentNode,
-                            currentComponent, componentClientId,
-                            (SelectItem) value);
+                    T t = processSelectItem(component, (SelectItem) value, true);
+                    if (t != null) {
+                        return t;
+                    }
 
                 } else if (value instanceof SelectItem[]) {
                     SelectItem selectItems[] = (SelectItem[]) value;
-                    for (int i = 0; i < selectItems.length; i++) {
-                        addSelectItem(parentNode, currentComponent,
-                                componentClientId, selectItems[i]);
+                    for (SelectItem selectItem : selectItems) {
+                        T t = processSelectItem(component, selectItem, false);
+                        if (t != null) {
+                            return t;
+                        }
                     }
 
                 } else if (value instanceof SelectItemGroup[]) {
-                    SelectItemGroup selectItems[] = (SelectItemGroup[]) value;
-                    for (int i = 0; i < selectItems.length; i++) {
-                        addSelectItem(parentNode, currentComponent,
-                                componentClientId, selectItems[i]);
+                    SelectItemGroup[] selectItems = (SelectItemGroup[]) value;
+                    for (SelectItem selectItem : selectItems) {
+                        T t = processSelectItem(component, selectItem, false);
+                        if (t != null) {
+                            return t;
+                        }
                     }
 
                 } else if ((value instanceof IFiltredCollection)
@@ -395,7 +220,7 @@ public class SelectItemTools {
                     Iterator< ? > it;
                     if (value instanceof IFiltredCollection2) {
                         it = ((IFiltredCollection2< ? >) value).iterator(
-                                currentComponent, filterProperties, -1);
+                                component, filterProperties, -1);
                     } else {
                         it = ((IFiltredCollection< ? >) value).iterator(
                                 filterProperties, -1);
@@ -412,8 +237,11 @@ public class SelectItemTools {
                                 }
                             }
 
-                            addSelectItem(parentNode, currentComponent,
-                                    componentClientId, (SelectItem) item);
+                            T t = processSelectItem(component,
+                                    (SelectItem) item, false);
+                            if (t != null) {
+                                return t;
+                            }
                         }
 
                     } finally {
@@ -431,63 +259,68 @@ public class SelectItemTools {
                         selectItem = new BasicSelectItem(uiSelectItem);
                     }
 
-                    singleItemNode = addSelectItem(parentNode,
-                            currentComponent, componentClientId, selectItem);
-                }
-
-                if (singleItemNode != null
-                        && currentComponent.getChildCount() > 0) {
-                    List<UIComponent> children = currentComponent.getChildren();
-                    for (UIComponent child : children) {
-
-                        listSelectItems(child, singleItemNode);
+                    T t = processSelectItem(component, selectItem, true);
+                    if (t != null) {
+                        return t;
                     }
                 }
 
-            } else if (currentComponent instanceof UISelectItems) {
-                Object value = ((UISelectItems) currentComponent).getValue();
+            } else if (component instanceof UISelectItems) {
+                Object value = ((UISelectItems) component).getValue();
+
                 if (value instanceof SelectItem[]) {
                     SelectItem selectItems[] = (SelectItem[]) value;
-                    for (int i = 0; i < selectItems.length; i++) {
-                        addSelectItem(parentNode, currentComponent,
-                                componentClientId, selectItems[i]);
+                    for (SelectItem selectItem : selectItems) {
+                        T t = processSelectItem(component, selectItem, false);
+                        if (t != null) {
+                            return t;
+                        }
                     }
                 }
             }
+
+            return null;
         }
 
-        private SelectItemNode addSelectItem(SelectItemNode parentNode,
-                UIComponent component, String componentClientId,
-                SelectItem selectItem) {
-
-            String selectItemId = componentClientId;
-
-            if (selectItemIds.add(componentClientId) == false) {
-                selectItemId = componentClientId + "::" + selectItemIds.size();
-                selectItemIds.add(componentClientId);
-            }
-
-            SelectItemNode node = parentNode.addChild(factory, selectItemId,
-                    selectItem);
+        private T processSelectItem(UIComponent component,
+                SelectItem selectItem, boolean searchComponentChildren) {
 
             if (handler != null) {
-                handler.beginNode(node);
+                T t = handler.beginNode(component, selectItem);
+                if (t != null) {
+                    return t;
+                }
             }
 
             if (selectItem instanceof SelectItemGroup) {
                 SelectItemGroup group = (SelectItemGroup) selectItem;
 
                 for (SelectItem child : group.getSelectItems()) {
-                    addSelectItem(node, component, componentClientId, child);
+                    processSelectItem(component, child, false);
                 }
 
             }
 
-            if (handler != null) {
-                handler.endNode(node);
+            if (searchComponentChildren && component.getChildCount() > 0) {
+                List<UIComponent> children = component.getChildren();
+                for (UIComponent child : children) {
+
+                    T t = processComponent(child);
+                    if (t != null) {
+                        return t;
+                    }
+                }
             }
 
-            return node;
+            if (handler != null) {
+                T t = handler.endNode(component, selectItem);
+                if (t != null) {
+                    return t;
+                }
+            }
+
+            return null;
         }
     }
+
 }
