389308dfcb19d9ff07473277d5e1bad65da0d8ce
oeuillot <>
2013-08-27 09:09:26 +0000

    *** empty log message ***

---
+++ org.rcfaces.core/src/org/rcfaces/core/component/capability/IAudioDescriptionCapability.java
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.core.component.capability;
+
+/**
+ * 
+ * @author Olivier Oeuillot (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public interface IAudioDescriptionCapability {
+    String getAudioDescription();
+
+    void setAudioDescription(String description);
+}
