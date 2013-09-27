b4fbe9143b9b55d5f33feeb3a5f1bf39a5750477
oeuillot <>
2013-09-27 13:32:29 +0000

    Nouveautés

---
+++ org.rcfaces.core/src/org/rcfaces/core/component/capability/IPageLoadedCapability.java
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.core.component.capability;
+
+import javax.faces.event.FacesListener;
+
+import org.rcfaces.core.event.IPageLoadedListener;
+
+public interface IPageLoadedCapability {
+
+    /**
+     * Adds a listener to the component for the load event
+     * 
+     * @param loadListener
+     *            the load listener to add
+     */
+    void addPageLoadedListener(IPageLoadedListener loadListener);
+
+    /**
+     * Removes a listener from the component for the load event
+     * 
+     * @param loadListener
+     *            the load listener to remove
+     */
+    void removePageLoadedListener(IPageLoadedListener loadListener);
+
+    /**
+     * Returns a list of load listener for the component
+     * 
+     * @return load listeners' list
+     */
+    FacesListener[] listPageLoadedListeners();
+
+}
