a195f2f8f43bce5bff31d549c04c2a3505fc1857
oeuillot <>
2013-11-29 14:26:32 +0000

    Conversion du systeme de positionnement des tooltips

---
+++ org.rcfaces.core/src/org/rcfaces/core/component/capability/IToolTipPositionCapability.java
@@ -0,0 +1,31 @@
+/*
+ * $Id$
+ */
+package org.rcfaces.core.component.capability;
+
+/**
+ * 
+ * @author Olivier Oeuillot (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public interface IToolTipPositionCapability {
+    String MOUSE_POSITION = "mouse";
+
+    String MIDDLE_COMPONENT_POSITION = "middle";
+
+    String BOTTOM_COMPONENT_POSITION = "bottom";
+
+    String LEFT_COMPONENT_POSITION = "left";
+
+    String BOTTOM_LEFT_COMPONENT_POSITION = "bottom-left";
+
+    String MIDDLE_LEFT_COMPONENT_POSITION = "middle-left";
+
+    String RIGHT_COMPONENT_POSITION = "right";
+
+    String BOTTOM_RIGHT_COMPONENT_POSITION = "bottom-right";
+
+    String MIDDLE_RIGHT_COMPONENT_POSITION = "middle-right";
+
+    String DEFAULT_POSITION = "default";
+}
